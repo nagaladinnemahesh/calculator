42fd931b599bb42f09dd32dd496133a9df6f8012
MaheshNagaladinne <maheshnagaladinne@gmail.com>
2023-12-06 12:20:41 +0530

    added addition function

--- calculator.sh
+++ calculator.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
 #this is a calculator application for practice
 
 # this will be used to practice branching strategies
 
+#Addition function
 
+addition() {
+    local num1=$1
+    local num2=$2
+    local result=$((num1 + num2))
+    echo "The sum of $num1 and $num2 is: $result"
+}
