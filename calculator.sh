2631b6f75cfd5042ecd10f1077e702074a31c2e8
MaheshNagaladinne <maheshnagaladinne@gmail.com>
2023-12-06 13:47:10 +0530

    added multiplication function

--- calculator.sh
+++ calculator.sh
@@ -33,4 +33,23 @@ read -p "Enter the first number: " num1
 read -p "Enter the second number: " num2
 
 # Call the subtraction function with user-input numbers
-subtraction "$num1" "$num2
+subtraction "$num1" "$num2"
+
+#Multiplication function
+multiplication() {
+    local num1
+    local num2
+
+    # Ask the user to input two numbers
+    read -p "Enter the first number: " num1
+    read -p "Enter the second number: " num2
+
+    # Perform multiplication
+    local result=$((num1 * num2))
+
+    # Display the result
+    echo "The product of $num1 and $num2 is: $result"
+}
+
+# Call the multiplication function
+multiplication
