690ce2d59b1667ef57d4ced2446f404cabd2100d
MaheshNagaladinne <maheshnagaladinne@gmail.com>
2023-12-06 13:37:31 +0530

    added substraction function

--- calculator.sh
+++ calculator.sh
@@ -19,3 +19,18 @@ read -p "Enter the second number: " num2
 # Call the addition function with user-input numbers
 addition "$num1" "$num2"
 
+#Substraction function
+
+subtraction() {
+    local num1=$1
+    local num2=$2
+    local result=$((num1 - num2))
+    echo "The result of subtracting $num2 from $num1 is: $result"
+}
+
+# Ask the user to input two numbers
+read -p "Enter the first number: " num1
+read -p "Enter the second number: " num2
+
+# Call the subtraction function with user-input numbers
+subtraction "$num1" "$num2
